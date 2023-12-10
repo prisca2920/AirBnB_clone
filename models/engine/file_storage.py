a1f955c74fb6564d4ef3c765f887009e612fd7c4
prisca2920 <prisca2920@gmail.com>
2023-12-10 09:41:38 -0800

    fstorage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,13 +2,6 @@
 """Creating the class that sores the model"""
 from os import path
 import json
-from models.base_model import BaseModel
-from models.amenity import Amenity
-from models.city import City
-from models.user import User
-from models.place import Place
-from models.review import Review
-from models.state import State
 
 
 class FileStorage:
@@ -34,6 +27,13 @@ class FileStorage:
 
     def reload(self):
         """loads saved dict from file"""
+        from models.base_model import BaseModel
+        from models.amenity import Amenity
+        from models.city import City
+        from models.user import User
+        from models.place import Place
+        from models.review import Review
+        from models.state import State
         if path.exists(self.__file_path):
             with open(self.__file_path, "r") as file:
                 for value in json.loads(file.read()).values():
